51cf2b20a76a8dbad5ebcd745f5b970816bf9758
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2019-03-10 11:56:47 +0400

    added base filter presenter

--- app/src/main/java/com/gnoemes/shikimori/presentation/presenter/search/FilterPresenter.kt
+++ app/src/main/java/com/gnoemes/shikimori/presentation/presenter/search/FilterPresenter.kt
@@ -9,20 +9,17 @@ import com.gnoemes.shikimori.entity.search.domain.FilterType
 import com.gnoemes.shikimori.entity.search.presentation.FilterAction
 import com.gnoemes.shikimori.entity.search.presentation.FilterViewModel
 import com.gnoemes.shikimori.entity.search.presentation.FilterWithButtonsViewModel
-import com.gnoemes.shikimori.presentation.presenter.base.BaseNetworkPresenter
+import com.gnoemes.shikimori.presentation.presenter.base.BaseFilterPresenter
 import com.gnoemes.shikimori.presentation.presenter.search.converter.FilterViewModelConverter
 import com.gnoemes.shikimori.presentation.view.search.filter.FilterView
 import com.gnoemes.shikimori.utils.clearAndAddAll
 import javax.inject.Inject
 
 @InjectViewState
-class FilterPresenter @Inject constructor(
+open class FilterPresenter @Inject constructor(
         private val interactor: FilterInteractor,
         private val converter: FilterViewModelConverter
-) : BaseNetworkPresenter<FilterView>() {
-
-    lateinit var type: Type
-    lateinit var appliedFilters: HashMap<String, MutableList<FilterItem>>
+) : BaseFilterPresenter<FilterView>() {
 
     private val items = mutableListOf<Any>()
 
@@ -43,7 +40,7 @@ class FilterPresenter @Inject constructor(
                 .addToDisposables()
     }
 
-    private fun loadData() {
+    override fun loadData() {
         (when (type) {
             Type.MANGA -> interactor.getMangaFilters()
             Type.RANOBE -> interactor.getRanobeFilters()
@@ -64,60 +61,34 @@ class FilterPresenter @Inject constructor(
         viewState.setSortFilters(it, if (selectedPos == -1) 1 else selectedPos)
     }
 
-    private fun onFiltersChanged() {
-        viewState.setResetEnabled(appliedFilters.size > 0)
-        loadData()
-    }
-
     fun onSortChanged(newSort: FilterItem) {
         clearAndAddToSelected(SearchConstants.ORDER, newSort)
     }
 
-    private fun clearAndAddToSelected(key: String, filterItem: FilterItem) {
-        clear(key)
-        addToSelected(key, filterItem)
-    }
-
     fun onFilterAction(type: FilterType, action: FilterAction) {
         when (action) {
-            is FilterAction.Clear -> clear(type.value)
-            is FilterAction.Invert -> invert(type.value)
-            is FilterAction.SelectAll -> select(type.value)
+            is FilterAction.Clear -> clearSection(type.value)
+            is FilterAction.Invert -> invertSection(type.value)
+            is FilterAction.SelectAll -> selectSection(type.value)
         }
     }
 
-    fun onFilterInverted(type: FilterType, item: FilterViewModel) {
-        when (item.state) {
-            FilterViewModel.STATE.INVERTED -> Unit
-            else -> invertOrAddNew(type.value, FilterItem(type.value, item.value, item.text))
-        }
-        onFiltersChanged()
-    }
-
-    fun onFilterSelected(type: FilterType, item: FilterViewModel) {
-        when (item.state) {
-            FilterViewModel.STATE.DEFAULT -> addToSelected(type.value, FilterItem(type.value, item.value, item.text))
-            else -> removeFromSelected(type.value, FilterItem(type.value, item.value, item.text))
-        }
-        onFiltersChanged()
-    }
-
-    fun onResetClicked() {
-        appliedFilters.clear()
-        onFiltersChanged()
+    override fun onResetClicked() {
+        super.onResetClicked()
         loadSortFilters()
     }
 
-    fun onAcceptClicked() {
-        viewState.onFiltersAccepted(appliedFilters)
+    private fun clearAndAddToSelected(key: String, filterItem: FilterItem) {
+        clearSection(key)
+        addToSelected(key, filterItem)
     }
 
-    private fun clear(key: String) {
+    private fun clearSection(key: String) {
         appliedFilters.remove(key)
         onFiltersChanged()
     }
 
-    private fun select(key: String) {
+    private fun selectSection(key: String) {
         appliedFilters[key] = items
                 .asSequence()
                 .filter { it is FilterWithButtonsViewModel }
@@ -129,7 +100,7 @@ class FilterPresenter @Inject constructor(
         onFiltersChanged()
     }
 
-    private fun invert(key: String) {
+    private fun invertSection(key: String) {
         appliedFilters[key] = items
                 .asSequence()
                 .filter { it is FilterWithButtonsViewModel }
@@ -141,39 +112,4 @@ class FilterPresenter @Inject constructor(
                 ?.toMutableList() ?: mutableListOf()
         onFiltersChanged()
     }
-
-    private fun getAppliedFilter(key: String, value: String?): FilterItem? {
-        return appliedFilters[key]?.find { it.value == value }
-    }
-
-    private fun invertOrAddNew(key: String, value: FilterItem) {
-        val item = getAppliedFilter(key, value.value)
-        item?.let { removeFromSelected(key, it); addInverted(key, it) } ?: addInverted(key, value)
-    }
-
-    private fun addInverted(key: String, item: FilterItem) {
-        val newItem =
-                if (!item.value?.contains("!")!!) item.copy(value = "!${item.value}")
-                else item
-        addToSelected(key, newItem)
-    }
-
-    private fun addToSelected(key: String, value: FilterItem) {
-        if (appliedFilters[key] != null && !appliedFilters[key]?.contains(value)!!) {
-            appliedFilters[key]?.add(value)
-        } else {
-            appliedFilters[key] = mutableListOf(value)
-        }
-    }
-
-    private fun removeFromSelected(key: String, value: FilterItem) {
-        if (appliedFilters[key]?.size == 1) {
-            appliedFilters.remove(key)
-        } else {
-            appliedFilters[key]?.remove(value)
-        }
-    }
-
-    private fun String.invertValue(): String = if (this.contains("!")) this.replace("!", "") else "!${this}"
-
 }
--- app/src/main/java/com/gnoemes/shikimori/presentation/view/search/filter/FilterView.kt
+++ app/src/main/java/com/gnoemes/shikimori/presentation/view/search/filter/FilterView.kt
@@ -1,18 +1,9 @@
 package com.gnoemes.shikimori.presentation.view.search.filter
 
-import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
-import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.gnoemes.shikimori.entity.common.domain.FilterItem
-import com.gnoemes.shikimori.presentation.view.base.fragment.BaseFragmentView
+import com.gnoemes.shikimori.presentation.view.base.fragment.BaseFilterView
 
-interface FilterView : BaseFragmentView {
-
-    fun showData(items : List<Any>)
-
-    fun setResetEnabled(show : Boolean)
-
-    @StateStrategyType(SkipStrategy::class)
-    fun onFiltersAccepted(appliedFilters: HashMap<String, MutableList<FilterItem>>)
+interface FilterView : BaseFilterView {
 
     fun setSortFilters(items: List<FilterItem>, selected : Int)
 }
