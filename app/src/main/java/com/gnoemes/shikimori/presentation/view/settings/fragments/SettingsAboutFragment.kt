167a0a3f90960607ec5e619b9073cf30f2d8c21a
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2019-03-13 21:19:42 +0400

    added link to forum

--- app/src/main/java/com/gnoemes/shikimori/presentation/view/settings/fragments/SettingsAboutFragment.kt
+++ app/src/main/java/com/gnoemes/shikimori/presentation/view/settings/fragments/SettingsAboutFragment.kt
@@ -5,7 +5,9 @@ import android.net.Uri
 import android.os.Bundle
 import com.gnoemes.shikimori.BuildConfig
 import com.gnoemes.shikimori.R
+import com.gnoemes.shikimori.entity.app.domain.Constants
 import com.gnoemes.shikimori.utils.preference
+import com.gnoemes.shikimori.utils.toUri
 
 class SettingsAboutFragment : BaseSettingsFragment() {
 
@@ -21,6 +23,14 @@ class SettingsAboutFragment : BaseSettingsFragment() {
                 val sendMail = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "gnoemes@gmail.com", null))
                 sendMail.putExtra(Intent.EXTRA_EMAIL, arrayOf("gnoemes@gmail.com"))
                 startActivity(Intent.createChooser(sendMail, null))
+                return@setOnPreferenceClickListener true
+            }
+        }
+
+        preference(R.string.settings_about_forum_key)?.apply {
+            setOnPreferenceClickListener {
+                val forum = Intent(Intent.ACTION_VIEW, Constants.FOUR_PDA_THEME_URL.toUri())
+                startActivity(forum)
                 return@setOnPreferenceClickListener true
             }
         }
