b0e544eec1c1c941fb0975a990076e8f205f5e2e
Dany <gnoemes@gmail.com>
2019-04-06 21:21:36 +0400

    fixed player for old devices

--- app/src/main/java/com/gnoemes/shikimori/presentation/view/player/embedded/EmbeddedPlayerActivity.kt
+++ app/src/main/java/com/gnoemes/shikimori/presentation/view/player/embedded/EmbeddedPlayerActivity.kt
@@ -105,7 +105,9 @@ class EmbeddedPlayerActivity : BaseActivity<EmbeddedPlayerPresenter, EmbeddedPla
         if (settingsSource.isOpenLandscape) requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         forwardView.text = smallOffsetText
+        forwardView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_forward, 0, 0)
         rewindView.text = smallOffsetText
+        rewindView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_rewind, 0, 0)
 
         exo_progress.setBufferedColor(ColorUtils.setAlphaComponent(colorAttr(R.attr.colorSecondaryTransparent), 153))
         resolutionSpinnerView.background.tint(color(R.color.player_controls))
