cf43712daaac6f3c499eba17e6315da7d784bd95
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2019-05-08 21:34:05 +0400

    fixed some themes issues

--- app/src/main/java/com/gnoemes/shikimori/presentation/view/common/fragment/EditRateFragment.kt
+++ app/src/main/java/com/gnoemes/shikimori/presentation/view/common/fragment/EditRateFragment.kt
@@ -108,9 +108,6 @@ class EditRateFragment : BaseBottomSheetDialogFragment() {
             }
         }
 
-        progressIncrementView.background = progressIncrementView.background.apply { tint(context!!.colorAttr(R.attr.colorPrimary)) }
-        progressDecrementView.background = progressDecrementView.background.apply { tint(context!!.colorAttr(R.attr.colorPrimary)) }
-
         progressIncrementView.setOnClickListener {
             val newValue = progressView.text?.toString()?.toIntOrNull()?.plus(1) ?: 0
             progressView.setText(newValue.toString())
