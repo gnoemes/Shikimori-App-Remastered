b0aa378086d57f8a6177af9723b4c26666279025
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2019-05-19 15:59:45 +0400

    fixed android 8+ rare ClassCastException issue

--- app/src/main/java/com/gnoemes/shikimori/entity/series/presentation/EpisodesNavigationData.kt
+++ app/src/main/java/com/gnoemes/shikimori/entity/series/presentation/EpisodesNavigationData.kt
@@ -1,10 +1,14 @@
 package com.gnoemes.shikimori.entity.series.presentation
 
+import android.os.Parcelable
 import com.gnoemes.shikimori.entity.common.domain.Image
+import kotlinx.android.parcel.Parcelize
 
-class EpisodesNavigationData(
-        animeId: Long,
-        image: Image,
-        name: String,
+
+@Parcelize
+data class EpisodesNavigationData(
+        private val _animeId: Long,
+        private val _image: Image,
+        private val _name: String,
         val rateId: Long?
-) : BaseSeriesNavigationData(animeId, image, name)
+) : BaseSeriesNavigationData(_animeId, _image, _name), Parcelable
