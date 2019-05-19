5eec6b0b04ce1b8e8cf1272d3ca28200b0fe643a
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2019-05-19 15:30:03 +0400

    fixed rate empty translation type

--- app/src/main/java/com/gnoemes/shikimori/entity/series/data/TranslationResponse.kt
+++ app/src/main/java/com/gnoemes/shikimori/entity/series/data/TranslationResponse.kt
@@ -9,7 +9,7 @@ data class TranslationResponse(
         @field:SerializedName("id") val id: Long,
         @field:SerializedName("animeId") val animeId: Long,
         @field:SerializedName("episodeId") val episodeId: Int,
-        @field:SerializedName("type") val type: TranslationType,
+        @field:SerializedName("type") private val _type: TranslationType?,
         @field:SerializedName("quality") val quality: TranslationQuality,
         @field:SerializedName("hosting") val _hosting: VideoHosting?,
         @field:SerializedName("author") val author: String,
@@ -17,4 +17,7 @@ data class TranslationResponse(
 ) {
     val hosting: VideoHosting
         get() = _hosting ?: VideoHosting.UNKNOWN
+
+    val type: TranslationType
+        get() = _type ?: TranslationType.VOICE_RU
 }
