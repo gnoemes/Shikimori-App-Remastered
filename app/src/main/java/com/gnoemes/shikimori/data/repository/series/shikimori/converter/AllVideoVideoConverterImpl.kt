7adde3f3b7c800154079b8d2abf76321537c9364
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-01-21 14:17:54 +0200

    Fix allvideo parsing when there is only one quality available.
    (cherry picked from commit 397ba2b86a56b05dc09faa5cca09508415b1f31c)

--- app/src/main/java/com/gnoemes/shikimori/data/repository/series/shikimori/converter/AllVideoVideoConverterImpl.kt
+++ app/src/main/java/com/gnoemes/shikimori/data/repository/series/shikimori/converter/AllVideoVideoConverterImpl.kt
@@ -12,6 +12,7 @@ class AllVideoVideoConverterImpl @Inject constructor() : AllVideoVideoConverter 
     override fun convertTracks(it: TranslationVideo, videos: AllVideoFileResponse): Video {
         val tracks = mutableListOf<Track>()
 
+        if (videos.unknown != null) tracks.add(Track("unknown", videos.unknown!!))
         if (videos.src1080 != null) tracks.add(Track("1080", videos.src1080!!))
         if (videos.src720 != null) tracks.add(Track("720", videos.src720!!))
         if (videos.src480 != null) tracks.add(Track("480", videos.src480!!))
@@ -22,7 +23,7 @@ class AllVideoVideoConverterImpl @Inject constructor() : AllVideoVideoConverter 
     }
 
     override fun parsePlaylists(html: String?): AllVideoFileResponse {
-        val fileResponse = AllVideoFileResponse(null, null, null, null, null)
+        val fileResponse = AllVideoFileResponse(null, null, null, null, null, null)
 
         if (html.isNullOrEmpty()) return fileResponse
 
@@ -36,8 +37,20 @@ class AllVideoVideoConverterImpl @Inject constructor() : AllVideoVideoConverter 
                 ?.split(",")
                 .orEmpty()
                 .forEach {
-                    val quality = it.substringAfter("[").substringBefore("]")
-                    val videoUrl = it.substringAfter("]")
+                    val quality: String
+                    val videoUrl: String
+
+                    val regex = Regex("\\[(\\d+p)\\](.+)")
+                    val match = regex.find(it)
+
+                    if (match == null) {
+                        quality = "unknown"
+                        videoUrl = it
+                    } else {
+                        val (q, url) = match.destructured
+                        quality = q
+                        videoUrl = url
+                    }
 
                     when (quality) {
                         "240p"-> fileResponse.src240 = videoUrl
@@ -45,6 +58,7 @@ class AllVideoVideoConverterImpl @Inject constructor() : AllVideoVideoConverter 
                         "480p"-> fileResponse.src480 = videoUrl
                         "720p"-> fileResponse.src720 = videoUrl
                         "1080p"-> fileResponse.src1080 = videoUrl
+                        "unknown"-> fileResponse.unknown = videoUrl
                     }
                 }
 
--- app/src/main/java/com/gnoemes/shikimori/entity/series/data/AllVideoFileResponse.kt
+++ app/src/main/java/com/gnoemes/shikimori/entity/series/data/AllVideoFileResponse.kt
@@ -5,5 +5,6 @@ data class AllVideoFileResponse(
         var src360 : String?,
         var src480 : String?,
         var src720 : String?,
-        var src1080 : String?
+        var src1080 : String?,
+        var unknown : String?
 )
